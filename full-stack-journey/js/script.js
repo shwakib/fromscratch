bf2ae846bbfecfa2869fda278764e2c7d3bfce85
shwakib <sifat6460@gmail.com>
2022-05-11 23:54:17 +0600

    secondday

---
+++ full-stack-journey/js/script.js
@@ -0,0 +1,37 @@
+
+// window.alert("Hello I am Sifat Hasan Wakib");
+// alert("Hello I am Sifat Hasan Wakib");
+document.write("Hello I am Sifat Hasan Wakib");
+document.getElementById("root").innerHTML = 'I Am learning Js!';
+document.getElementById("root1").innerHTML = 'I Am learning node!';
+document.getElementById("root2").innerHTML = 'I Am learning mongo!';
+console.log("See you not for mind");
+var x;
+// x=prompt("Enter Your Name:");
+// console.log(x);
+// document.write(x);
+let b;
+b="Adib";
+alert(b);
+var c=20;
+var d=c;
+console.log(c);
+console.log(d);
+c=30;
+console.log(c);
+console.log(d);
+var e=c+d;
+console.log(e);
+const a=20;
+// a=3;
+var line1="HEllo ";
+var line2="world";
+var line3=line1+" "+line2;
+console.log(line3);
+var f=line1+34+4;
+console.log(f);
+
+var tempinc=prompt("Enter temperature in celsius:");
+var tempinf=(9/5*tempinc+32);
+alert("Temperature in Farenheit: "+tempinf+" degree");
+console.log("Task Complete");
