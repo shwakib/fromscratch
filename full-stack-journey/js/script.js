0e9e9c749f89db04de2ce4be34cda2da526f6f52
shwakib <sifat6460@gmail.com>
2022-05-14 16:52:25 +0600

    template literals + condistions

--- full-stack-journey/js/script.js
+++ full-stack-journey/js/script.js
@@ -12,7 +12,7 @@ var x;
 // document.write(x);
 let b;
 b="Adib";
-alert(b);
+//alert(b);
 var c=20;
 var d=c;
 console.log(c);
@@ -41,9 +41,45 @@ let u=v;
 v=45;
 console.log(u);
 
+//Primitive & Refrence
 let arr=[1,2,3];
 let newArr=arr;
 console.log(newArr);
 arr[0]=500;
 console.log(arr);
 console.log(newArr);
+
+//Template Literals
+let lines=`First line
+Second line`;
+console.log(lines);
+let age=38;
+console.log(`His age is ${age}`);
+
+let name1="adib";
+let age1=28;
+let address="Dhaka";
+console.log(`His name is  ${name1}
+His age is ${age1}
+His address is ${address}`);
+
+let n1=65;
+let n2=60;
+console.log(`The summation of ${n1}+${n2} is ${n1+n2}`);
+var age2=prompt("Enter You arge:");
+if(age2>=70)
+{
+    console.log("Over 70");
+}
+else if(age2>=50 && age2<70)
+{
+    console.log("Below 70 but over 50");
+}
+else if(age2>=18 && age2<50)
+{
+    console.log("Between 18-50")
+}
+else{
+    alert("Invalid Input");
+}
+console.log("End of If statement");
