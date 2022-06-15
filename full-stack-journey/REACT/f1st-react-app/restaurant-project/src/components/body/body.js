3f357c4a5a2c4cdba926041b47e8e97cf568389c
shwakib <sifat6460@gmail.com>
2022-06-16 01:22:42 +0600

    routes added

--- full-stack-journey/REACT/f1st-react-app/restaurant-project/src/components/body/body.js
+++ full-stack-journey/REACT/f1st-react-app/restaurant-project/src/components/body/body.js
@@ -3,11 +3,18 @@ import Menus from "./Menus";
 import Home from "./home";
 import About from "./about";
 import Contact from "./contact";
+import { Route, Navigate, Routes } from "react-router-dom";
 
 const Body = () => {
     return (
         <div>
-            <Menus />
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/menu" element={<Menus />} />
+                <Route path="/contact" exact element={<Contact />} />
+                <Route path="/about" exact element={<About />} />
+                <Route path="/" element={<Navigate to="/menu" />} />
+            </Routes>
         </div>
     )
 }
