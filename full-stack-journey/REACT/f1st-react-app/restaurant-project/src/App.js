b4a2b545c1247466fcfc1e20e449aedc5a29aaf4
shwakib <sifat6460@gmail.com>
2022-06-18 16:27:32 +0600

    redux store added

--- full-stack-journey/REACT/f1st-react-app/restaurant-project/src/App.js
+++ full-stack-journey/REACT/f1st-react-app/restaurant-project/src/App.js
@@ -1,13 +1,17 @@
 import './App.css';
 import MainComponent from './components/MainComponent.js';
-import { BrowserRouter } from 'react-router-dom'
+import { BrowserRouter } from 'react-router-dom';
+import myStore from './redux/store';
+import { Provider } from 'react-redux';
 
 function App() {
   return (
     <div className="App">
-      <BrowserRouter>
-        <MainComponent />
-      </BrowserRouter>
+      <Provider store={myStore}>
+        <BrowserRouter>
+          <MainComponent />
+        </BrowserRouter>
+      </Provider>
     </div>
   );
 }
---
+++ full-stack-journey/REACT/redux_basic/index.js
@@ -0,0 +1,50 @@
+const redux = require('redux');
+
+//State
+const initState = {
+    num: 0
+}
+
+//Reducer
+const rootReducer = (state = initState, action) => {
+    if (action.type === 'INC_NUM') {
+        return {
+            ...state,
+            num: state.num + 1
+        }
+    }
+    if (action.type === 'ADD_NUM') {
+        return {
+            ...state,
+            num: state.num + action.value
+        }
+    }
+    if (action.type === 'DEC_NUM') {
+        return {
+            ...state,
+            num: state.num - 10
+        }
+    }
+    return state;
+}
+
+//Store
+const store = redux.createStore(rootReducer);
+console.log(store.getState());
+
+//Subscribe
+store.subscribe(() => {
+    console.log("[Subscribe]", store.getState());
+})
+
+//Dispatching Action
+store.dispatch({
+    type: 'INC_NUM'
+});
+store.dispatch({
+    type: 'ADD_NUM',
+    value: 34
+});
+store.dispatch({
+    type: 'DEC_NUM'
+});
