aef8d6e1cf938b9168f59c98fabe8d095c53317a
shwakib <sifat6460@gmail.com>
2022-06-20 00:35:32 +0600

    using font-awesome

--- full-stack-journey/REACT/f1st-react-app/restaurant-project/src/components/body/home.js
+++ full-stack-journey/REACT/f1st-react-app/restaurant-project/src/components/body/home.js
@@ -1,12 +1,13 @@
 import React, { Component } from "react";
 import { connect } from 'react-redux';
+import Loading from './loading';
 
 class Home extends Component {
     render() {
         document.title = "Bohubrihi Restaurant";
         return (
             <div>
-
+                <Loading />
             </div>
         )
     }
---
+++ full-stack-journey/REACT/f1st-react-app/restaurant-project/src/components/body/loading.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const loading = () => {
+    return (
+        <div className="col-12" style={{ padding: "60px" }}>
+            <span className="fa fa-spinner fa-5x text-success fa-fw fa-pulse"></span>
+        </div >
+    )
+}
+
+export default loading;
