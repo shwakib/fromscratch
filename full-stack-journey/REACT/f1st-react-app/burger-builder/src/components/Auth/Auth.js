d86867c2f0bdf816ca1786bff4380d500fba884a
shwakib <sifat6460@gmail.com>
2022-07-07 23:26:14 +0600

    validation added

--- full-stack-journey/REACT/f1st-react-app/burger-builder/src/components/Auth/Auth.js
+++ full-stack-journey/REACT/f1st-react-app/burger-builder/src/components/Auth/Auth.js
@@ -12,18 +12,46 @@ class Auth extends Component {
                 }
                     onSubmit={
                         (values) => {
-                            console.log(values);
+                            console.log("values: ", values);
                         }
                     }
+                    validate={(values) => {
+                        const errors = {};
+
+                        if (!values.email) {
+                            errors.email = 'Required';
+                        }
+                        else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)) {
+                            errors.email = 'Invalid Email Address';
+                        }
+
+                        if (!values.password) {
+                            errors.password = 'Required';
+                        }
+                        else if (values.password.length < 4) {
+                            errors.password = 'Must be at least 4 characters';
+                        }
+
+                        if (!values.passwordConfirm) {
+                            errors.passwordConfirm = 'Required';
+                        }
+                        else if (values.password !== values.passwordConfirm) {
+                            errors.passwordConfirm = 'Password field does not match';
+                        }
+
+                        //console.log(errors);
+                        return errors;
+                    }}
                 >
-                    {({ values, handleChange, handleSubmit }) => (
-                        <div>
+                    {({ values, handleChange, handleSubmit, errors }) => (
+                        <div style={{ border: "1px grey solid", padding: "15px", borderRadius: "7px" }}>
                             <form onSubmit={handleSubmit}>
                                 <input name='email' type='text' placeholder='Enter Your E-mail' className='form-control' value={values.email} onChange={handleChange} />
+                                <span style={{ color: "red" }}>{errors.email}</span>
                                 <br />
-                                <input name='password' type='password' placeholder='Password' className='form-control' value={values.password} onChange={handleChange} />
+                                <input name='password' type='password' placeholder='Password' className='form-control' value={values.password} onChange={handleChange} /> <span style={{ color: "red" }}>{errors.password}</span>
                                 <br />
-                                <input name='passwordConfirm' type='password' placeholder='Enter Your Password Again' className='form-control' value={values.passwordConfirm} onChange={handleChange} />
+                                <input name='passwordConfirm' type='password' placeholder='Enter Your Password Again' className='form-control' value={values.passwordConfirm} onChange={handleChange} /> <span style={{ color: "red" }}>{errors.passwordConfirm}</span>
                                 <br />
                                 <button type='submit' className='btn btn-success'>Sign Up</button>
                             </form>
