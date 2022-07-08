5fe2722bd9483ae5ac7e1e36ac3f83f68c3cfa8d
shwakib <sifat6460@gmail.com>
2022-07-08 12:22:23 +0600

    switch between sign up & login

--- full-stack-journey/REACT/f1st-react-app/burger-builder/src/components/Auth/Auth.js
+++ full-stack-journey/REACT/f1st-react-app/burger-builder/src/components/Auth/Auth.js
@@ -2,6 +2,14 @@ import React, { Component } from 'react';
 import { Formik } from 'formik';
 
 class Auth extends Component {
+    state = {
+        mode: "Sign Up",
+    }
+    switchModeHandler = () => {
+        this.setState({
+            mode: this.state.mode === "Sign Up" ? "Login" : "Sign Up"
+        })
+    }
     render() {
         return (
             <div>
@@ -32,28 +40,31 @@ class Auth extends Component {
                             errors.password = 'Must be at least 4 characters';
                         }
 
-                        if (!values.passwordConfirm) {
-                            errors.passwordConfirm = 'Required';
+                        if (this.state.mode === "Sign Up") {
+                            if (!values.passwordConfirm) {
+                                errors.passwordConfirm = 'Required';
+                            }
+                            else if (values.password !== values.passwordConfirm) {
+                                errors.passwordConfirm = 'Password field does not match';
+                            }
                         }
-                        else if (values.password !== values.passwordConfirm) {
-                            errors.passwordConfirm = 'Password field does not match';
-                        }
-
                         //console.log(errors);
                         return errors;
                     }}
                 >
                     {({ values, handleChange, handleSubmit, errors }) => (
                         <div style={{ border: "1px grey solid", padding: "15px", borderRadius: "7px" }}>
+                            <button style={{ width: "100%", backgroundColor: "#D70F64", color: "white" }} className="btn btn-lg" onClick={this.switchModeHandler}>Switch to {this.state.mode === "Sign Up" ? "Login" : "Sign Up"}</button>
+                            <br /><br />
                             <form onSubmit={handleSubmit}>
                                 <input name='email' type='text' placeholder='Enter Your E-mail' className='form-control' value={values.email} onChange={handleChange} />
                                 <span style={{ color: "red" }}>{errors.email}</span>
                                 <br />
                                 <input name='password' type='password' placeholder='Password' className='form-control' value={values.password} onChange={handleChange} /> <span style={{ color: "red" }}>{errors.password}</span>
                                 <br />
-                                <input name='passwordConfirm' type='password' placeholder='Enter Your Password Again' className='form-control' value={values.passwordConfirm} onChange={handleChange} /> <span style={{ color: "red" }}>{errors.passwordConfirm}</span>
-                                <br />
-                                <button type='submit' className='btn btn-success'>Sign Up</button>
+                                {this.state.mode === "Sign Up" ? <div><input name='passwordConfirm' type='password' placeholder='Enter Your Password Again' className='form-control' value={values.passwordConfirm} onChange={handleChange} /> <span style={{ color: "red" }}>{errors.passwordConfirm}</span>
+                                    <br /></div> : null}
+                                <button type='submit' className='btn btn-success'>{this.state.mode === "Sign Up" ? "Sign Up" : "Log in"}</button>
                             </form>
                         </div>
                     )}
