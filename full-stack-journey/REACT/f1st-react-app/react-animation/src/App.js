4f3ad7b90a2ad1699d5afd0fe05a7d03bee3a34d
shwakib <sifat6460@gmail.com>
2022-07-12 14:02:23 +0600

    timing and event

--- full-stack-journey/REACT/f1st-react-app/react-animation/src/App.js
+++ full-stack-journey/REACT/f1st-react-app/react-animation/src/App.js
@@ -7,22 +7,26 @@ class App extends Component {
     show: true
   }
   render() {
+    const animateTime = {
+      enter: 1000, exit: 5000
+    }
     return (
       <div className="App">
         <button onClick={() => this.setState({ show: !this.state.show })}>Toggle</button>
-        <Transition in={this.state.show} timeout={2000}>
+        <Transition in={this.state.show} timeout={animateTime} mountOnEnter unmountOnExit onEnter={() => console.log("OnEnter")} onEntering={() => console.log("OnEntering")} onEntered={() => console.log("OnEntered")} onExit={() => console.log("onExit")} onExiting={() => console.log("OnExiting")} onExited={() => console.log("OnExited")}>
           {
             mode => (<div style={{
               background: "black",
               color: "white",
               borderRadius: 4,
               transition: 'opacity 1s ease-out',
-              opacity: mode === "exited" ? 0 : 1
+              opacity: mode === "exiting" || mode == "entering" ? 0 : 1
             }}>
               <p>I am some text!</p>
             </div>)
           }
         </Transition>
+        <p>I am outside of the Transition!</p>
       </div >
     );
   }
