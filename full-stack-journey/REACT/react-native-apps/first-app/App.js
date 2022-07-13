77a92fe20c029f37eff04db3ee2cf645717cd948
shwakib <sifat6460@gmail.com>
2022-07-13 11:53:55 +0600

    react-native-components

--- full-stack-journey/REACT/react-native-apps/first-app/App.js
+++ full-stack-journey/REACT/react-native-apps/first-app/App.js
@@ -1,10 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TextInput } from 'react-native';
 
 export default function App() {
+  const handleInputChange = text => {
+    alert(text);
+  }
   return (
     <View style={styles.container}>
       <Text>Hello! I am Sifat Hasan Wakib</Text>
+      <TextInput style={{ height: 40, borderColor: 'gray', borderWidth: 1 }} placeholder="I Am A Placeholder" onChangeText={text => handleInputChange(text)} />
       <StatusBar style="auto" />
     </View>
   );
