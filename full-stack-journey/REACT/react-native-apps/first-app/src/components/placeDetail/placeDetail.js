d12730382ccbdeb30f1858b32d85a27bd1440c1c
shwakib <sifat6460@gmail.com>
2022-07-26 22:55:38 +0600

    eject with icon setup

--- full-stack-journey/REACT/react-native-apps/first-app/src/components/placeDetail/placeDetail.js
+++ full-stack-journey/REACT/react-native-apps/first-app/src/components/placeDetail/placeDetail.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { View, Modal, Image, Text, Button } from 'react-native';
+import Icon from 'react-native-vector-icons/FontAwesome5';
 
 const placeDetail = props => {
     return (
@@ -8,6 +9,7 @@ const placeDetail = props => {
                 <Image source={props.place.image} style={{ width: "100%", height: 300 }} />
                 <Text style={{ textAlign: "center", fontSize: 40, fontWeight: "bold" }}>{props.place.value}</Text>
                 <View>
+                    <Icon name="trash" size={20} color="red" />
                     <Button title="Delete" color="red" onPress={() => props.handleDeleteItem(props.place.key)} />
                     <Button title="Close" onPress={() => props.handleModalClose()} />
                 </View>
