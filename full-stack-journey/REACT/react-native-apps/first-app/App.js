60584ff86a7f1967a57fa71358667124357d8db3
shwakib <sifat6460@gmail.com>
2022-07-27 01:14:10 +0600

    setting up Navigation

--- full-stack-journey/REACT/react-native-apps/first-app/App.js
+++ full-stack-journey/REACT/react-native-apps/first-app/App.js
@@ -1,12 +1,22 @@
+import 'react-native-gesture-handler';
 import React, { useState } from 'react';
 import MainComponent from './src/mainComponent';
 import { Provider } from 'react-redux';
 import Store from './src/redux/store';
+import { NavigationContainer } from '@react-navigation/native';
+import Login from './src/components/Login/login';
+import { createStackNavigator } from '@react-navigation/stack';
+
+const Stack = createStackNavigator();
 
 export default function App() {
   return (
-    <Provider store={Store}>
-      <MainComponent />
-    </Provider>
+    <NavigationContainer>
+      <Provider store={Store}>
+        <Stack.Navigator>
+          <Stack.Screen name='Login' component={Login} />
+        </Stack.Navigator>
+      </Provider>
+    </NavigationContainer>
   )
 }
