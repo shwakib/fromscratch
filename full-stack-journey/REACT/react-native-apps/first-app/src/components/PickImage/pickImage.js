8e0474eb80a0ae49381d8098d72d835d0d20a233
shwakib <sifat6460@gmail.com>
2022-07-28 23:03:14 +0600

    image picker complete

--- full-stack-journey/REACT/react-native-apps/first-app/src/components/PickImage/pickImage.js
+++ full-stack-journey/REACT/react-native-apps/first-app/src/components/PickImage/pickImage.js
@@ -1,10 +1,27 @@
 import React from "react";
-import { View, Button } from 'react-native'
+import { View, Button, Image } from 'react-native';
+import * as ImagePicker from 'expo-image-picker'
 
 const PickImage = props => {
+    const handleImagePick = async () => {
+        try {
+            let result = await ImagePicker.launchImageLibraryAsync({ mediaTypes: ImagePicker.MediaTypeOptions.All, allowsEditing: true, aspect: [4, 3], quality: 1 });
+            if (!result.cancelled) {
+                props.setImage(result.uri);
+            }
+        }
+        catch (E) {
+            console.log(E);
+        }
+    }
+    let showImage = null;
+    if (props.image !== "") {
+        showImage = <Image source={{ uri: props.image }} style={{ width: "100%", height: 200, marginBottom: 10 }} />
+    }
     return (
         <View>
-            <Button title="Pick An Image" />
+            {showImage}
+            <Button title="Pick An Image" onPress={handleImagePick} />
         </View>
     )
 }
--- full-stack-journey/REACT/react-native-apps/first-app/src/components/SharePlaces/SharePlaces.js
+++ full-stack-journey/REACT/react-native-apps/first-app/src/components/SharePlaces/SharePlaces.js
@@ -13,16 +13,17 @@ const mapDispatchToProps = dispatch => {
 
 const SharePlaces = props => {
     const [inputValue, setInputValue] = useState(""); //State defined
+    const [image, setImage] = useState("");
     return (
         <View>
-            <PickImage />
+            <PickImage image={image} setImage={setImage} />
             <InputPlace
                 inputValue={inputValue} setInputValue={setInputValue} />
             <View style={{ alignItems: "center" }}>
                 <Button title='Add Place' onPress={() => {
                     if (inputValue !== "") {
                         props.addPlace({
-                            key: Math.random().toString(), value: props.inputValue, image: {
+                            key: Math.random().toString(), value: inputValue, image: {
                                 uri: "https://cdn.britannica.com/97/189797-050-1FC0041B/Night-view-Dhaka-Bangladesh.jpg"
                             }
                         })
