1904fe49f8ccc5139f991ed61f0d4dc0ca476255
shwakib <sifat6460@gmail.com>
2022-08-02 00:19:42 +0600

    redux added

--- full-stack-journey/REACT/react-native-apps/restaurant-project/App.js
+++ full-stack-journey/REACT/react-native-apps/restaurant-project/App.js
@@ -1,12 +1,16 @@
 import 'react-native-gesture-handler';
 import React from "react";
 import AppNavigator from "./app/AppNavigator";
-import { NavigationContainer } from '@react-navigation/native'
+import { NavigationContainer } from '@react-navigation/native';
+import { Provider } from 'react-redux';
+import { store } from './app/redux/store'
 
 export default function App() {
   return (
-    <NavigationContainer>
-      <AppNavigator />
-    </NavigationContainer>
+    <Provider store={store}>
+      <NavigationContainer>
+        <AppNavigator />
+      </NavigationContainer>
+    </Provider>
   );
 }
