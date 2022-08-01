fca8ff31032445961a3dda44b71b878fd3446ffb
shwakib <sifat6460@gmail.com>
2022-08-01 18:41:32 +0600

    setting up new-project

---
+++ full-stack-journey/REACT/react-native-apps/restaurant-project/app/AppNavigator.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { View, Text } from 'react-native';
+
+const AppNavigator = () => {
+    return (
+        <View><Text>AppNavigator Screen</Text></View>
+    )
+}
+
+export default AppNavigator;
---
+++ full-stack-journey/REACT/react-native-apps/restaurant-project/app/screens/DishDetail.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { View, Text } from 'react-native';
+
+const DishDetail = () => {
+    return (
+        <View><Text>DIsh Detail Screen</Text></View>
+    )
+}
+
+export default DishDetail;
