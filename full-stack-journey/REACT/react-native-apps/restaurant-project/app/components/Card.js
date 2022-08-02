06242505b5258cb61fe28e50553ba00c12e97f99
shwakib <sifat6460@gmail.com>
2022-08-03 01:59:02 +0600

    delete on long press-react native-end

--- full-stack-journey/REACT/react-native-apps/restaurant-project/app/components/Card.js
+++ full-stack-journey/REACT/react-native-apps/restaurant-project/app/components/Card.js
@@ -1,22 +1,40 @@
 import React from "react";
-import { View, StyleSheet, Image, Text } from 'react-native';
+import { View, StyleSheet, Image, Text, Pressable, Alert } from 'react-native';
 import { connect } from "react-redux";
 import { removeFavouriteDish } from "../redux/actionCreators";
 
 const mapDispatchToProps = dispatch => {
     return {
-        removeFavouriteDish: dish => removeFavouriteDish(dish)
+        removeFavouriteDish: dish => dispatch(removeFavouriteDish(dish))
     }
 }
 
 const Card = props => {
+    const removeFavDish = () => {
+        Alert.alert(
+            'Delete favourite?',
+            'Are you sure to delete ' + props.item.name + ' from your favourite list?',
+            [
+                {
+                    text: 'Cancel',
+                    onPress: () => console.log("Cancelled"),
+                    style: "cancel"
+                },
+                {
+                    onPress: () => props.removeFavouriteDish(props.item),
+                },
+            ]
+        )
+    }
     return (
-        <View style={styles.card}>
-            <Image source={{ uri: props.item.image }} style={styles.image} />
-            <View style={styles.details}>
-                <Text style={styles.title}>{props.item.name}</Text>
+        <Pressable onLongPress={() => removeFavDish()}>
+            <View style={styles.card}>
+                <Image source={{ uri: props.item.image }} style={styles.image} />
+                <View style={styles.details}>
+                    <Text style={styles.title}>{props.item.name}</Text>
+                </View>
             </View>
-        </View>
+        </Pressable>
     )
 }
 
