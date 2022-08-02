926ce9e97f85e6439f56ac860b1e7826654cc763
shwakib <sifat6460@gmail.com>
2022-08-02 17:42:34 +0600

    styling Menuscreen

--- full-stack-journey/REACT/react-native-apps/restaurant-project/app/components/MenuItem.js
+++ full-stack-journey/REACT/react-native-apps/restaurant-project/app/components/MenuItem.js
@@ -4,14 +4,40 @@ import { View, StyleSheet, Image, TouchableHighlight, Text } from 'react-native'
 const MenuItem = props => {
     return (
         <TouchableHighlight>
-            <View>
-                <View>
-                    <Text>{props.item.name}</Text>
-                    <Text>{props.item.description}</Text>
+            <View style={styles.container}>
+                {props.item.image && <Image source={{ uri: props.item.image }} style={styles.image} />}
+                <View style={styles.details}>
+                    <Text style={styles.name}>{props.item.name}</Text>
+                    <Text style={styles.description} numberOfLines={3}>{props.item.description}</Text>
                 </View>
             </View>
         </TouchableHighlight>
     )
 }
+
+const styles = StyleSheet.create({
+    container: {
+        alignItems: "center",
+        flexDirection: "row",
+        padding: 15
+    },
+    details: {
+        flex: 1,
+        marginLeft: 10,
+        justifyContent: "center"
+    },
+    image: {
+        width: 80,
+        height: 80,
+        borderRadius: 10
+    },
+    name: {
+        fontWeight: "500"
+    },
+    description: {
+        color: "#6e6969",
+        textAlign: "justify"
+    }
+})
 
 export default MenuItem;
