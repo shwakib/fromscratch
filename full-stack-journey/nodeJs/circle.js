e670160b952b1a8621da0862be2b49b727747d93
shwakib <sifat6460@gmail.com>
2022-08-03 17:18:42 +0600

    creating module

---
+++ full-stack-journey/nodeJs/circle.js
@@ -0,0 +1,9 @@
+const area = r => 3.14 * r * r;
+const circumference = r => 2 * 3.14 * r;
+const message = "I am in Bangladesh";
+
+module.exports.area = area;
+module.exports.circumference = circumference;
+exports.msg = message
+
+console.log(module);
