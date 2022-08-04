90e688c0e2addc353ed7b4b027ed5bdf46e647bd
shwakib <sifat6460@gmail.com>
2022-08-04 23:11:49 +0600

    Events Module

--- full-stack-journey/nodeJs/app.js
+++ full-stack-journey/nodeJs/app.js
@@ -6,9 +6,29 @@
 // console.log(circleApp.circumference(3));
 // console.log(area(3));
 
+
+//File System Module
 const fs = require('fs');
 
 let text = fs.readFileSync('./files/input.txt', 'utf-8');
 
 text = `Copied Text: ${text}`;
 fs.writeFileSync('./files/output.txt', text);
+
+//Events Module
+// addEventListener('click',(e)=>{
+
+// })
+const EventEmitter = require('events');
+const emitter = new EventEmitter();
+
+emitter.on('event1', (e) => {
+    console.log("EVENT 1 called", e);
+})
+
+emitter.on('event2', () => {
+    console.log("EVENT 2 called");
+})
+
+emitter.emit('event1', { name: "Sifat Hasan", id: 1 });
+emitter.emit('event2');
