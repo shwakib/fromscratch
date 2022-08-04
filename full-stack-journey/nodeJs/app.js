03992f3250eaa9c7ec1b56515050cdca35649e6b
shwakib <sifat6460@gmail.com>
2022-08-04 23:01:38 +0600

    File System Module (Synchronous)

--- full-stack-journey/nodeJs/app.js
+++ full-stack-journey/nodeJs/app.js
@@ -1,7 +1,14 @@
-const circleApp = require("./circle");
+// const circleApp = require("./circle");
 
-console.log("Loaded Module:", circleApp);
-// console.log(circleApp.area(3));
-console.log(circleApp.msg);
-console.log(circleApp.circumference(3));
-console.log(area(3));
+// console.log("Loaded Module:", circleApp);
+// // console.log(circleApp.area(3));
+// console.log(circleApp.msg);
+// console.log(circleApp.circumference(3));
+// console.log(area(3));
+
+const fs = require('fs');
+
+let text = fs.readFileSync('./files/input.txt', 'utf-8');
+
+text = `Copied Text: ${text}`;
+fs.writeFileSync('./files/output.txt', text);
--- full-stack-journey/nodeJs/circle.js
+++ full-stack-journey/nodeJs/circle.js
@@ -1,10 +1,10 @@
-const area = r => 3.14 * r * r;
-const circumference = r => 2 * 3.14 * r;
-const message = "I am in Bangladesh";
+// const area = r => 3.14 * r * r;
+// const circumference = r => 2 * 3.14 * r;
+// const message = "I am in Bangladesh";
 
-module.exports.area = area;
-module.exports.circumference = circumference;
-exports.msg = message;
-module.exports = area;
+// module.exports.area = area;
+// module.exports.circumference = circumference;
+// exports.msg = message;
+// module.exports = area;
 
-console.log(module);
+// console.log(module);
