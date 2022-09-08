9cbf8190cba13a7e3d3a5cd4b10ef61276a7d06a
shwakib <sifat6460@gmail.com>
2022-09-09 01:06:44 +0600

    Asynchronous pattern

--- full-stack-journey/nodeJs/npm-app/index.js
+++ full-stack-journey/nodeJs/npm-app/index.js
@@ -3,3 +3,17 @@ const cir = (r) => 2 * 3.14 * r;
 
 module.exports.area = area;
 module.exports.cir = cir;
+
+//AsyncJS
+
+console.log("Line 1");
+const students = getStudents(1);
+console.log(students);
+console.log("Line 2");
+
+function getStudents(id) {
+    setTimeout(() => {
+        console.log("Fetching from database...");
+        return { id: id, name: "Rahim" }
+    }, 2000);
+}
