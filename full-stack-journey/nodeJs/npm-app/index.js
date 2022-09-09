08bc472c5d7b1112d01be06e7e8ad0ba527823ff
shwakib <sifat6460@gmail.com>
2022-09-09 17:01:57 +0600

    Async await

--- full-stack-journey/nodeJs/npm-app/index.js
+++ full-stack-journey/nodeJs/npm-app/index.js
@@ -8,10 +8,25 @@ module.exports.cir = cir;
 
 console.log("Line 1");
 
-getStudents(3)
-    .then(students => getCourses(students))
-    .then(courses => getQuizMarks(courses.courses))
-    .then(marks => console.log(marks))
+// getStudents(3)
+//     .then(students => getCourses(students))
+//     .then(courses => getQuizMarks(courses.courses))
+//     .then(marks => console.log(marks))
+//     .catch(err => console.log(err));
+
+//Async & Await
+async function studentData() {
+    try {
+        const student = await getStudents(4);
+        const courses = await getCourses(student);
+        const marks = await getQuizMarks(courses.courses);
+        console.log(marks);
+    } catch (err) {
+        console.log(err);
+    }
+}
+
+studentData();
 
 console.log("Line 2");
 
@@ -32,8 +47,9 @@ function getCourses(student) {
             console.log("Student Courses from database...");
             const courses = { id: student.id, name: student.name, courses: ["Javascript", "Python"] }
             console.log(courses);
-            resolve(courses), 5500
-        })
+            resolve(courses);
+            // reject("Error Occured");
+        }, 5500);
     })
 }
 
@@ -41,7 +57,7 @@ function getQuizMarks(courses) {
     return new Promise((resolve, reject) => {
         setTimeout(() => {
             console.log("Courses Marks are loading...");
-            resolve({ [courses[0]]: 90, [courses[1]]: 85 }), 7000
-        })
+            resolve({ [courses[0]]: 90, [courses[1]]: 85 })
+        }, 7000);
     })
 }
