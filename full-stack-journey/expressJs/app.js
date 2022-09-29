0ef53477896940c76dc51ecf01b214b688e09551
shwakib <sifat6460@gmail.com>
2022-09-29 22:48:28 +0600

    Writing custom middleware

--- full-stack-journey/expressJs/app.js
+++ full-stack-journey/expressJs/app.js
@@ -3,7 +3,20 @@ const app = express();
 const studentRouter = require('./routers/studentRouter');
 
 app.use(express.json());
+app.use((req, res, next) => {
+    console.log("I am middleware 1");
+    next();
+});
+app.use((req, res, next) => {
+    console.log("I am middleware 2");
+    next();
+});
 app.use('/api/students', studentRouter);
+
+app.get('/', (request, response, next) => {
+    // response.send("Another Response!");
+    next();
+})
 
 app.get('/', (request, response) => {
     response.send("Hello from expressJs!");
