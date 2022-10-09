17b0463f0aa5b341781357d2c63c0f72c479c3e3
shwakib <sifat6460@gmail.com>
2022-10-10 01:01:10 +0600

    creating model & saving

--- full-stack-journey/mongoDB/index.js
+++ full-stack-journey/mongoDB/index.js
@@ -5,7 +5,7 @@ mongoose.connect('mongodb://localhost:27017/my-students')
     .catch(err => console.error("Connection Failed!!"));
 
 //Schema-> Define the shape of documents
-const studnetSchema = new mongoose.Schema({
+const studentSchema = new mongoose.Schema({
     firstName: { type: String },
     lastName: String,
     dob: Date,
@@ -18,3 +18,22 @@ const studnetSchema = new mongoose.Schema({
     },
     subjects: [{ name: String, marks: { type: Number, min: 0, max: 100 } }]
 })
+
+//Model
+const Student = mongoose.model('Student', studentSchema); //Class
+const student = new Student({
+    firstName: "Sifat Hasan",
+    lastName: "Wakib",
+    dob: new Date("31 December 1998"),
+    passed: true,
+    hobbies: ["Swimming", "Singing"],
+    parents: {
+        fatherName: "A",
+        motherName: "B"
+    },
+    subjects: [{ name: "Math", marks: 80 }, { name: "English", marks: 90 }]
+});
+
+student.save()
+    .then(data => console.log(data))
+    .catch(err => console.log(err._message));
