2893ae8221c8cb0c751c0775c0b6331ccffac729
shwakib <sifat6460@gmail.com>
2022-10-10 00:16:41 +0600

    schema

--- full-stack-journey/mongoDB/index.js
+++ full-stack-journey/mongoDB/index.js
@@ -3,3 +3,18 @@ const mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost:27017/my-students')
     .then(() => console.log("Connected to Database successfully"))
     .catch(err => console.error("Connection Failed!!"));
+
+//Schema-> Define the shape of documents
+const studnetSchema = new mongoose.Schema({
+    firstName: { type: String },
+    lastName: String,
+    dob: Date,
+    entryDate: { type: Date, default: Date.now },
+    passed: Boolean,
+    hobbies: [String],
+    parents: {
+        fatherName: String,
+        motherName: String
+    },
+    subjects: [{ name: String, marks: { type: Number, min: 0, max: 100 } }]
+})
