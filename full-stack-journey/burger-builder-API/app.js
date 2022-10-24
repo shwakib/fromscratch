f62389ad3ca5ef5d2e111fdb6e86c013d90d2d9c
shwakib <sifat6460@gmail.com>
2022-10-24 11:20:16 +0600

    settign up project

--- full-stack-journey/burger-builder-API/app.js
+++ full-stack-journey/burger-builder-API/app.js
@@ -2,9 +2,11 @@ const express = require('express');
 const cors = require('cors');
 const userRouter = require('./routers/userRouter');
 const orderRouter = require('./routers/orderRouter');
+const compression = require('compression');
 
 const app = express();
 
+app.use(compression());
 app.use(cors());
 app.use(express.json());
 
