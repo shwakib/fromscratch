1b8f811fd873895bc1faa4612b4d028b8eafe2d4
shwakib <sifat6460@gmail.com>
2022-10-26 21:41:41 +0600

    user router & sign up

---
+++ full-stack-journey/E-Commerce/e-com-backend/controllers/userControllers.js
@@ -0,0 +1,33 @@
+const bcrypt = require('bcrypt');
+const _ = require('lodash');
+const { User, validate } = require('../models/user');
+
+module.exports.signUp = async (req, res) => {
+    const { error } = validate(req.body);
+    if (error) return res.status(400).send(error.details[0].message);
+
+    let user = {};
+    user = await User.findOne({ email: req.body.email });
+    if (user) return res.status(400).send('User already registered!');
+
+    user = new User(_.pick(req.body, ['name', 'email', 'password']));
+
+    const salt = await bcrypt.genSalt(10);
+    user.password = await bcrypt.hash(user.password, salt);
+    const token = user.generateJWT();
+
+    try {
+        const result = await user.save();
+        return res.status(201).send({
+            message: "Registration Successful!",
+            token: token,
+            user: _.pick(result, ["_id", "name", "email"])
+        })
+    } catch (err) {
+        return res.status(500).send("Server Error");
+    }
+}
+
+module.exports.signIn = async (req, res) => {
+
+}
