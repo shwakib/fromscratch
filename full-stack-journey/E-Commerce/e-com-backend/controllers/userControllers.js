eb359001f72352ffbc6d91c928f4d3491888653f
shwakib <sifat6460@gmail.com>
2022-10-26 22:04:33 +0600

    user authentication

--- full-stack-journey/E-Commerce/e-com-backend/controllers/userControllers.js
+++ full-stack-journey/E-Commerce/e-com-backend/controllers/userControllers.js
@@ -29,5 +29,16 @@ module.exports.signUp = async (req, res) => {
 }
 
 module.exports.signIn = async (req, res) => {
+    let user = await User.findOne({ email: req.body.email });
+    if (!user) return res.status(400).send("Invalid E-mail or password!");
 
+    const validUser = await bcrypt.compare(req.body.password, user.password);
+    if (!validUser) return res.status(400).send("Invalid E-mail or password!");
+
+    const token = user.generateJWT();
+    return res.status(200).send({
+        message: "Login Successful!",
+        token: token,
+        user: _.pick(user, ["_id", "name", "email"])
+    })
 }
