d045716d8144d41e4f73467af737532967e05998
shwakib <sifat6460@gmail.com>
2022-10-30 00:41:09 +0600

    CRUD in Cart

---
+++ full-stack-journey/E-Commerce/e-com-backend/controllers/cartController.js
@@ -0,0 +1,40 @@
+const { CartItem } = require('../models/cartItem');
+const _ = require('lodash');
+
+module.exports.createCartItem = async (req, res) => {
+    let { price, product } = _.pick(req.body, ["price", "product"]);
+    let item = await CartItem.findOne({
+        user: req.user._id,
+        product: product
+    });
+    if (item) return res.status(400).send("Item already exists in Cart!!!");
+    let cartItem = new CartItem({
+        price: price, product: product, user: req.user._id
+    });
+    const result = await cartItem.save();
+    return res.status(201).send({
+        message: "Added to Cart Successfully",
+        data: result
+    })
+}
+
+module.exports.getCartItem = async (req, res) => {
+    const cartItems = await CartItem.find({
+        user: req.user._id
+    }).populate('product', 'name').populate('user', 'name');
+    return res.status(200).send(cartItems);
+}
+
+module.exports.updateCartItem = async (req, res) => {
+    const { _id, count } = _.pick(req.body, ["count", "_id"]);
+    userId = req.user._id;
+    const result = await CartItem.updateOne({ _id: _id, user: userId }, { count: count });
+    return res.status(200).send("Item updated");
+}
+
+module.exports.deleteCartItem = async (req, res) => {
+    const _id = req.params.id;
+    const userId = req.user._id;
+    await CartItem.deleteOne({ _id: _id, user: userId });
+    return res.status(200).send("Deleted");
+}
