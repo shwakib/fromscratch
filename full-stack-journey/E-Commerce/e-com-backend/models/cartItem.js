8024499d61108ea937983f624adb722382b8da97
shwakib <sifat6460@gmail.com>
2022-10-29 23:33:43 +0600

    cart model

---
+++ full-stack-journey/E-Commerce/e-com-backend/models/cartItem.js
@@ -0,0 +1,11 @@
+const { Schema, model } = require('mongoose');
+
+const cartItemSchema = Schema({
+    product: { type: Schema.Types.ObjectId, ref: "Product", required: true },
+    price: Number,
+    count: { type: Number, default: 1, min: 1, max: 5 },
+    user: { type: Schema.Types.ObjectId, ref: "User", required: true }
+}, { timestamps: true });
+
+module.exports.CartItemSchema = this.CartItemSchema;
+module.exports.CartItem = model("CartItem", cartItemSchema);
