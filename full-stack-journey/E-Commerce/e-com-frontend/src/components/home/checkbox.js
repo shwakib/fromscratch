07826471cb1b3894f237fec5a81ca12fd5770a17
shwakib <sifat6460@gmail.com>
2022-11-06 23:35:40 +0600

    filter products-category -2

--- full-stack-journey/E-Commerce/e-com-frontend/src/components/home/checkbox.js
+++ full-stack-journey/E-Commerce/e-com-frontend/src/components/home/checkbox.js
@@ -1,9 +1,26 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 const Checkbox = ({ categories }) => {
+    const [checked, setChecked] = useState([]);
+    const checkedIds = [...checked];
+
+    const handleToggle = id => () => {
+        const foundId = checked.indexOf(id);
+        if (foundId === -1) {
+            checkedIds.push(id);
+        } else {
+            checkedIds.splice(foundId, 1);
+        }
+        setChecked(checkedIds);
+    }
+
+    useEffect(() => {
+        alert(JSON.stringify(checked))
+    }, [checked])
+
     return categories.map(category => (
         <li className="list-unstyled" key={category._id}>
-            <input type="checkbox" className="form-check-input" />
+            <input type="checkbox" className="form-check-input" onChange={handleToggle(category._id)} value={checked.indexOf(category._id === -1)} />
             <label className="form-check-label">{category.name}</label>
         </li>
     ))
