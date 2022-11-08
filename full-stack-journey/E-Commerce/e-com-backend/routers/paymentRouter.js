a63e5ffb01b9205f0f21551dac6e54a81d5693e1
shwakib <sifat6460@gmail.com>
2022-11-08 23:30:12 +0600

    payment Controller- Part 1

---
+++ full-stack-journey/E-Commerce/e-com-backend/routers/paymentRouter.js
@@ -0,0 +1,6 @@
+const router = require('express').Router();
+const { initPayment } = require('../controllers/paymentController');
+const authorize = require('../middlewares/authorize');
+
+router.route('/')
+    .get(authorize, initPayment);
