15538bf8f42520cc11de082497142ea380cf0434
shwakib <sifat6460@gmail.com>
2022-11-10 01:24:58 +0600

    Accpt response -IPN URL

--- full-stack-journey/E-Commerce/e-com-backend/controllers/paymentController.js
+++ full-stack-journey/E-Commerce/e-com-backend/controllers/paymentController.js
@@ -23,7 +23,7 @@ module.exports.initPayment = async (req, res) => {
         success: "yoursite.com/success", // If payment Succeed
         fail: "yoursite.com/fail", // If payment failed
         cancel: "yoursite.com/cancel", // If user cancel payment
-        ipn: "yoursite.com/ipn", // SSLCommerz will send http post request in this link
+        ipn: "https://safe-ocean-93615.herokuapp.com/api/payment/ipn", // SSLCommerz will send http post request in this link
     });
 
     // Set order details
