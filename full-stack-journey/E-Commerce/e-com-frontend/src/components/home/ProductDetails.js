893434c9215c48e53fa328181f75a804fd99c643
shwakib <sifat6460@gmail.com>
2022-11-16 20:34:24 +0600

    star added

--- full-stack-journey/E-Commerce/e-com-frontend/src/components/home/ProductDetails.js
+++ full-stack-journey/E-Commerce/e-com-frontend/src/components/home/ProductDetails.js
@@ -6,6 +6,7 @@ import { Link, useParams } from 'react-router-dom';
 import { getProductDetails } from '../../api/apiProducts';
 import { addToCart, updateCartCount } from '../../api/apiOrder';
 import { isAuthenticated, userInfo } from '../../utils/auth';
+import * as Star from '../../App.css';
 
 const ProductDetails = (props) => {
     const [product, setProducts] = useState({});
@@ -83,9 +84,28 @@ const ProductDetails = (props) => {
                         <span style={{ marginLeft: 10 }}>[ {product.soldUnit} units sold out of {product.quantity} units ]</span>
                     </p>
                     <p>{product.description}</p>
-                    {product.quantity ? <>
-                        &nbsp;<button className="btn btn-outline-primary btn-md" onClick={handleAddToCart(product)}>Add to Cart</button>
-                    </> : ""}
+                    {product.quantity ? <>&nbsp;<button className="btn btn-outline-primary btn-md" onClick={handleAddToCart(product)}>Add to Cart</button></> : ""}
+                    <div>
+                        <h3 style={{ padding: 4 }}>Please add a review for the product</h3>
+                        <div className={Star}>
+                            <div class="rate">
+                                <input type="radio" id="star5" name="rate" value="5" />
+                                <label for="star5" title="text">5 stars</label>
+                                <input type="radio" id="star4" name="rate" value="4" />
+                                <label for="star4" title="text">4 stars</label>
+                                <input type="radio" id="star3" name="rate" value="3" />
+                                <label for="star3" title="text">3 stars</label>
+                                <input type="radio" id="star2" name="rate" value="2" />
+                                <label for="star2" title="text">2 stars</label>
+                                <input type="radio" id="star1" name="rate" value="1" />
+                                <label for="star1" title="text">1 star</label>
+                            </div>
+                        </div>
+                        <div >
+                            <label htmlFor='reviewdescription'></label>
+                            <textarea name='reviewdescription'></textarea>
+                        </div>
+                    </div>
                 </div>
             </div>
         </Layout>
