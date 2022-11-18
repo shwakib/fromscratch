265197d6fa7c467fa403f66e232d982cf5a0d1da
shwakib <sifat6460@gmail.com>
2022-11-18 16:08:06 +0600

    review input

--- full-stack-journey/E-Commerce/e-com-frontend/src/components/home/ProductDetails.js
+++ full-stack-journey/E-Commerce/e-com-frontend/src/components/home/ProductDetails.js
@@ -55,6 +55,36 @@ const ProductDetails = (props) => {
         }
     }
 
+    const reviewSection = () => {
+        return (
+            <div>
+                <h3 style={{ padding: 4 }}>Please add a review for the product</h3>
+                <div className='row '>
+                    <div style={{ marginLeft: "5px" }} className={Star}>
+                        <div class="rate" >
+                            <input type="radio" id="star5" name="rate" value="5" />
+                            <label for="star5" title="text">5 stars</label>
+                            <input type="radio" id="star4" name="rate" value="4" />
+                            <label for="star4" title="text">4 stars</label>
+                            <input type="radio" id="star3" name="rate" value="3" />
+                            <label for="star3" title="text">3 stars</label>
+                            <input type="radio" id="star2" name="rate" value="2" />
+                            <label for="star2" title="text">2 stars</label>
+                            <input type="radio" id="star1" name="rate" value="1" />
+                            <label for="star1" title="text">1 star</label>
+                        </div>
+                    </div>
+                </div>
+                <div className='row'>
+                    <div style={{ marginLeft: "20px" }}>
+                        <label htmlFor='reviewdescription'></label>
+                        <textarea name='reviewdescription'></textarea>
+                    </div>
+                </div>
+            </div>
+        )
+    }
+
     return (
         <Layout title='Product Page'>
             <nav aria-label="breadcrumb">
@@ -85,27 +115,7 @@ const ProductDetails = (props) => {
                     </p>
                     <p>{product.description}</p>
                     {product.quantity ? <>&nbsp;<button className="btn btn-outline-primary btn-md" onClick={handleAddToCart(product)}>Add to Cart</button></> : ""}
-                    <div>
-                        <h3 style={{ padding: 4 }}>Please add a review for the product</h3>
-                        <div className={Star}>
-                            <div class="rate">
-                                <input type="radio" id="star5" name="rate" value="5" />
-                                <label for="star5" title="text">5 stars</label>
-                                <input type="radio" id="star4" name="rate" value="4" />
-                                <label for="star4" title="text">4 stars</label>
-                                <input type="radio" id="star3" name="rate" value="3" />
-                                <label for="star3" title="text">3 stars</label>
-                                <input type="radio" id="star2" name="rate" value="2" />
-                                <label for="star2" title="text">2 stars</label>
-                                <input type="radio" id="star1" name="rate" value="1" />
-                                <label for="star1" title="text">1 star</label>
-                            </div>
-                        </div>
-                        <div >
-                            <label htmlFor='reviewdescription'></label>
-                            <textarea name='reviewdescription'></textarea>
-                        </div>
-                    </div>
+                    {reviewSection()}
                 </div>
             </div>
         </Layout>
