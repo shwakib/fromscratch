3da89d55629f2ee72477184159c619325f19675e
shwakib <sifat6460@gmail.com>
2022-11-30 01:13:25 +0600

    authorization added for history

--- full-stack-journey/E-Commerce/e-com-backend/routers/userRouter.js
+++ full-stack-journey/E-Commerce/e-com-backend/routers/userRouter.js
@@ -1,10 +1,14 @@
 const router = require('express').Router();
-const { signIn, signUp } = require('../controllers/userControllers');
+const { signIn, signUp, fetchPurchaseHistory } = require('../controllers/userControllers');
+const authorize = require('../middlewares/authorize');
 
 router.route('/signup')
     .post(signUp);
 
 router.route('/signin')
     .post(signIn);
+
+router.route('/purchasehistory')
+    .post(authorize, fetchPurchaseHistory);
 
 module.exports = router;
