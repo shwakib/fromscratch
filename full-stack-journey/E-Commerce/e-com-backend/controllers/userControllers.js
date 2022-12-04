87fb3a027e76538cb231d52c2b60a887e1339bee
shwakib <sifat6460@gmail.com>
2022-12-05 00:31:15 +0600

    change

--- full-stack-journey/E-Commerce/e-com-backend/controllers/userControllers.js
+++ full-stack-journey/E-Commerce/e-com-backend/controllers/userControllers.js
@@ -42,6 +42,6 @@ module.exports.signIn = async (req, res) => {
 
 module.exports.fetchPurchaseHistory = async (req, res) => {
     let user = req.body.userID;
-    const result = await Order.find({ user: user }).populate('products', 'name');
+    const result = await Order.find({ user: user }).populate('product', 'name');
     return res.status(200).send(result);
 }
