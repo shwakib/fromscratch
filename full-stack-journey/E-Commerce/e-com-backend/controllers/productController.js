9699d0cda71f363e6af1e7de0d380e63f31ba8e6
shwakib <sifat6460@gmail.com>
2022-12-06 00:53:13 +0600

    update

--- full-stack-journey/E-Commerce/e-com-backend/controllers/productController.js
+++ full-stack-journey/E-Commerce/e-com-backend/controllers/productController.js
@@ -63,7 +63,7 @@ module.exports.getProductsById = async (req, res) => {
 
 module.exports.getProductName = async (req, res) => {
     const productId = req.params.id;
-    const result = await Product.findById(productId).select({ name: 1 });
+    const result = await Product.findById(productId).select({ photo: 0 });
     return res.status(200).send(result);
 }
 
