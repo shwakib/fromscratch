038da375a64e49a53309171d31da430a0048fe60
shwakib <sifat6460@gmail.com>
2022-12-09 00:16:06 +0600

    Pay Now button added for pending order

--- full-stack-journey/E-Commerce/e-com-frontend/src/components/user/OrderHistory.js
+++ full-stack-journey/E-Commerce/e-com-frontend/src/components/user/OrderHistory.js
@@ -15,7 +15,7 @@ const OrderHistory = ({ item, serial, viewDetails }) => {
             <td>{getCartTotal()}</td>
             <td>{item.payment_status}</td>
             <td align="left">{item.address.address1},{item.address.address2}. <br />Phone: {item.address.phone} </td>
-            <td><button className="btn btn-primary btn-sm" onClick={viewDetails}>View Product Details</button></td>
+            <td>{item.payment_status === "Complete" ? <button className="btn btn-primary btn-sm" onClick={viewDetails}>View Product Details</button> : <div><button className="btn btn-success btn-sm" /*onClick={viewDetails}*/>Pay Now</button><button style={{ margin: "10px 0px" }} className="btn btn-primary btn-sm" onClick={viewDetails}>View Product Details</button></div>}</td>
         </tr>
     )
 };
